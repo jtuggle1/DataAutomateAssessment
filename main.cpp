5be2780753be7c7a7e3978a69d1141953b821662
CPP Master <jtuggle1@asu.edu>
2024-06-08 12:39:45 -0700

    quick code assessment

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 #include <sstream>
+
 
 using namespace std;
 
 string decode(const string& message_file) {
+    //declaring necessary data values
     string r;
+    int linecount = 0;
+    map<int, string> mymap;
     ifstream file(message_file);
     string str;
-    while (getline(file, str)) {
+    int j = 1;
 
-        cout << "Read line: " << str << endl;
+
+    //get the line count and place every key value pair into a map
+    while (getline(file, str)) {
+    istringstream iss(str);
+    int n;
+    string w;
+    linecount++;
+    iss>> n >> w;
+    mymap[n] = w;
+    }
+
+
+    //adding to the string only the words at the end of each pyramid line
+    for (int i = 1; i <= linecount;){
+        r += mymap[i] + " ";
+        j++;
+        i = i + j;
     }
 
     return r;
 }
 
 int main() {
-    string decoded_message = decode("C:/Users/cppma/OneDrive/Desktop/test.txt");
+    string decoded_message = decode("C:/Users/cppma/OneDrive/Desktop/coding_qual_input.txt");
     cout << "Decoded Message:\n" << decoded_message << endl;
     return 0;
 }
